9edc0bd79c72304df9427a147a6904262a273f9e
Pedro Masson <pedro.masson@inovafarma.com.br>
2022-12-30 10:51:44 -0300

    feat: create entities

---
+++ Todo.Domain/Entities/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Todo.Domain.Entities
+{
+    public abstract class Entity : IEquatable<Entity>
+    {
+        protected Entity() 
+            => Id = Guid.NewGuid();
+
+        public bool Equals(Entity otherEntity)
+            => Id == otherEntity.Id;
+
+        public Guid Id { get; private set; }
+    }
+}
---
+++ Todo.Domain/Entities/TodoItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Todo.Domain.Entities
+{
+    public class TodoItem : Entity
+    {
+        public TodoItem(string title, DateTime date, string user)
+        {
+            Title = title;
+            Done = false;
+            Date = date;
+            User = user;
+        }
+
+        public string Title { get; private set; }
+        public bool Done { get; private set; }
+        public DateTime Date { get; private set; }
+        public string User { get; private set; }
+
+        public void MarkAsDone()
+            => Done = true;
+
+        public void MarkAsOnGoing()
+            => Done = false;
+
+        public void DefineTitle(string title)
+            => Title = title;
+    }
+}
