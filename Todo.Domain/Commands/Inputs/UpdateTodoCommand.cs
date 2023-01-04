b15b6d4e78b7afcd902f2e6705df636c22d6a095
Pedro Masson <pedro.masson@inovafarma.com.br>
2023-01-04 09:26:36 -0300

    feat: add update command

---
+++ Todo.Domain/Commands/Inputs/UpdateTodoCommand.cs
@@ -0,0 +1,37 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System;
+using Todo.Domain.Commands.Inputs.Contracts;
+
+namespace Todo.Domain.Commands.Inputs
+{
+    public class UpdateTodoCommand : Notifiable<Notification>, ICommand
+    {
+        public UpdateTodoCommand() { }
+
+        public UpdateTodoCommand(Guid id, string title, string user)
+        {
+            Id = id;
+            Title = title;
+            User = user;
+        }
+
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string User { get; set; }
+
+        public bool Validate()
+        {
+            AddNotifications
+            (
+                new Contract<Notification>()
+                    .Requires()
+                    .IsNull(Id, "Id", "Your Id cannot be null!")
+                    .IsNotNullOrEmpty(Title, "Title", "Your title cannot be null nor empty!")
+                    .IsLowerOrEqualsThan(Title, 5, "Please create a better description for this task!")
+            );
+
+            return IsValid;
+        }
+    }
+}
