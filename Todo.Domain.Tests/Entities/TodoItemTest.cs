6c58eb62ac219f5c1e0906677e84085e1563d400
Pedro Masson <pedro.masson@inovafarma.com.br>
2023-01-04 17:49:08 -0300

    test: create tests for todo item entity

---
+++ Todo.Domain.Tests/Entities/TodoItemTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Tests.Entities
+{
+    [TestClass]
+    public class TodoItemTest
+    {
+        [TestMethod]
+        public void Todo_title_must_be_equal() 
+        {
+            //Arrange
+            var sut = new TodoItem("Do the laundry", DateTime.Now, "Pedro");
+
+            //Act
+            sut.UpdateTitle("Play basketball");
+
+            //Assert
+            Assert.AreEqual(sut.Title, "Play basketball");
+        }
+    }
+}
