5d443a07b007b959cd460ca7f43f1598ee78c60e
Pedro Masson <pedro.masson@inovafarma.com.br>
2023-01-06 17:47:26 -0300

    feat: implements repository and update the interface adding methods

--- Todo.Domain/Repositories/ITodoRepository.cs
+++ Todo.Domain/Repositories/ITodoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Todo.Domain.Entities;
 
 namespace Todo.Domain.Repositories
@@ -8,5 +9,10 @@ namespace Todo.Domain.Repositories
         void Create(TodoItem todo);
         void Update(TodoItem todo);
         TodoItem GetTodoById(Guid? id, string user);
+
+        IEnumerable<TodoItem> GetAll(string user);
+        IEnumerable<TodoItem> GetAllDone(string user);
+        IEnumerable<TodoItem> GetAllUndone(string user);
+        IEnumerable<TodoItem> GetAllByPeriod(string user, bool done, DateTime date);
     }
 }
---
+++ Todo.Infra/Repositories/TodoRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Todo.Domain.Entities;
+using Todo.Domain.Repositories;
+
+namespace Todo.Infra.Repositories
+{
+    public class TodoRepository : ITodoRepository
+    {
+        public void Create(TodoItem todo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TodoItem> GetAll(string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TodoItem> GetAllByPeriod(string user, bool done, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TodoItem> GetAllDone(string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TodoItem> GetAllUndone(string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TodoItem GetTodoById(Guid? id, string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TodoItem todo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
