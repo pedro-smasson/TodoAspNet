14c6ffc216cf1ea3a2bbc302bbe8e037f0ebf3f6
Pedro Masson <pedro.masson@inovafarma.com.br>
2023-01-06 17:46:52 -0300

    fix: implements dependency injection and access to the ef core in memory database

--- Todo.Api/Startup.cs
+++ Todo.Api/Startup.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Todo.Domain.Commands.Handlers;
+using Todo.Domain.Repositories;
+using Todo.Infra.Contexts;
+using Todo.Infra.Repositories;
 
 namespace Todo.Api
 {
@@ -22,13 +20,20 @@ namespace Todo.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("Database"));
+
+            services.AddTransient<ITodoRepository, TodoRepository>();
+
+            services.AddTransient<CreateTodoHandler, CreateTodoHandler>();
+            services.AddTransient<UpdateTodoHandler, UpdateTodoHandler>();
+            services.AddTransient<MarkTodoAsDoneHandler, MarkTodoAsDoneHandler>();
+            services.AddTransient<MarkTodoAsUndoneHandler, MarkTodoAsUndoneHandler>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
