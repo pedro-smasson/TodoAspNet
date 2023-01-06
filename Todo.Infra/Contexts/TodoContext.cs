9fe7839d32ea506c4caf19e07c8eab4f1d8597d7
Pedro Masson <pedro.masson@inovafarma.com.br>
2023-01-06 09:41:18 -0300

    feat: create context and mapping props for sql

---
+++ Todo.Infra/Contexts/TodoContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Entities;
+
+namespace Todo.Infra.Contexts
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+        public DbSet<TodoItem> Todos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<TodoItem>().Property(x => x.Id);
+            modelBuilder.Entity<TodoItem>().Property(x => x.User).HasMaxLength(100).HasColumnType("varchar");
+            modelBuilder.Entity<TodoItem>().Property(x => x.Title).HasMaxLength(100).HasColumnType("varchar");
+            modelBuilder.Entity<TodoItem>().Property(x => x.Done).HasColumnType("bit");
+            modelBuilder.Entity<TodoItem>().Property(x => x.Date);
+            modelBuilder.Entity<TodoItem>().HasIndex(u => u.User);
+        }
+    }
+}
