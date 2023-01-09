8113f56bba8ad212517598b4936a99aaf66354bc
Pedro Masson <pedro.masson@inovafarma.com.br>
2023-01-09 10:05:26 -0300

    feat: create todo controller for http requests

---
+++ Todo.Api/Controllers/TodoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Todo.Domain.Commands.Handlers;
+using Todo.Domain.Commands.Inputs;
+using Todo.Domain.Entities;
+using Todo.Domain.Repositories;
+
+namespace Todo.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/todos")]
+    public class TodoController : ControllerBase
+    {
+        [Route("")]
+        [HttpPost]
+        public GenericCommandResult Create([FromBody] CreateTodoCommand command, [FromServices] CreateTodoHandler handler) 
+        {
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll([FromServices] ITodoRepository repository) 
+        {
+            return repository.GetAll("Pedro");
+        }
+    }
+}
