9f5b445fbc9a4a7cdeb15dc0d5ba3a3e470219df
Pedro Masson <pedro.masson@inovafarma.com.br>
2023-01-09 17:41:51 -0300

    fix: corrects ef migrations package bug

--- Todo.Infra/Contexts/TodoContext.cs
+++ Todo.Infra/Contexts/TodoContext.cs
@@ -5,12 +5,14 @@ namespace Todo.Infra.Contexts
 {
     public class TodoContext : DbContext
     {
+        public TodoContext() { }
         public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
 
         public DbSet<TodoItem> Todos { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<TodoItem>().ToTable("Todos");
             modelBuilder.Entity<TodoItem>().Property(x => x.Id);
             modelBuilder.Entity<TodoItem>().Property(x => x.User).HasMaxLength(100).HasColumnType("varchar");
             modelBuilder.Entity<TodoItem>().Property(x => x.Title).HasMaxLength(100).HasColumnType("varchar");
@@ -18,5 +20,8 @@ namespace Todo.Infra.Contexts
             modelBuilder.Entity<TodoItem>().Property(x => x.Date);
             modelBuilder.Entity<TodoItem>().HasIndex(u => u.User);
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Data Source=DESKTOP-IETNHSD;Initial Catalog=Todos;User ID=sa; pwd=123");
     }
 }
---
+++ Todo.Infra/Migrations/20230109184433_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Todo.Infra.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    Done = table.Column<bool>(type: "bit", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    User = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todos", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Todos_User",
+                table: "Todos",
+                column: "User");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todos");
+        }
+    }
+}
