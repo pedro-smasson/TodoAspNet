080a24b07a16d59ebdf783c4550f1baecfe2ab55
Pedro Masson <pedro.masson@inovafarma.com.br>
2023-01-09 08:40:49 -0300

    fix: fixed build bugs

--- Todo.Domain.Tests/Repositories/FakeTodoRepository.cs
+++ Todo.Domain.Tests/Repositories/FakeTodoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Todo.Domain.Entities;
 using Todo.Domain.Repositories;
 
@@ -7,6 +8,18 @@ namespace Todo.Domain.Tests.Repositories
     public class FakeTodoRepository : ITodoRepository
     {
         public void Create(TodoItem todo) { }
+
+        public IEnumerable<TodoItem> GetAll(string user)
+            { yield return new TodoItem("Lorem Ipsum", DateTime.Now, "Pedro"); }
+
+        public IEnumerable<TodoItem> GetAllByPeriod(string user, bool done, DateTime date)
+            { yield return new TodoItem("Lorem Ipsum", DateTime.Now, "Pedro"); }
+
+        public IEnumerable<TodoItem> GetAllDone(string user)
+            { yield return new TodoItem("Lorem Ipsum", DateTime.Now, "Pedro"); }
+
+        public IEnumerable<TodoItem> GetAllUndone(string user)
+            { yield return new TodoItem("Lorem Ipsum", DateTime.Now, "Pedro"); }
 
         public TodoItem GetTodoById(Guid? id, string user) { return new TodoItem("Lorem Ipsum", DateTime.Now, "Pedro"); }
 
