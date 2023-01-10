c0bca18aeed666baed14bf140db61a27003dd2ca
Pedro Masson <pedro.masson@inovafarma.com.br>
2023-01-10 09:24:11 -0300

    feat: add configuration for firebase and jwt authentication

--- Todo.Api/Startup.cs
+++ Todo.Api/Startup.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Todo.Domain.Commands.Handlers;
 using Todo.Domain.Repositories;
 using Todo.Infra.Contexts;
@@ -33,6 +35,22 @@ namespace Todo.Api
             services.AddTransient<UpdateTodoHandler, UpdateTodoHandler>();
             services.AddTransient<MarkTodoAsDoneHandler, MarkTodoAsDoneHandler>();
             services.AddTransient<MarkTodoAsUndoneHandler, MarkTodoAsUndoneHandler>();
+
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt =>
+                {
+                    opt.Authority = "https://securetoken.google.com/todoappaspnet";
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = "https://securetoken.google.com/todoappaspnet",
+                        ValidateAudience = true,
+                        ValidAudience = "todoappaspnet",
+                        ValidateLifetime = true
+                    };
+                });
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
